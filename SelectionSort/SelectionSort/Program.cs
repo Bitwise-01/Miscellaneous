601e6bdd6d126211115e24796630b665a997476a
Mohamed <Ethical.Hacker720@gmail.com>
2018-03-01 10:18:29 -0500

    Selection Sort In C#

---
+++ SelectionSort/SelectionSort/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * 
+ * Date: 02/28/2018
+ * Author: Mohamed Sheikh
+ * Description: Selection Sort
+ * 
+ */
+
+using System;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            sSort n = new sSort();
+            n.Sort();
+           
+            Console.WriteLine(n.ToString());
+            Console.ReadLine();
+            
+        }
+    }
+}
---
+++ SelectionSort/SelectionSort/sSort.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * 
+ * Date: 02/28/2018
+ * Author: Mohamed Sheikh
+ * Description: Selection Sort
+ * 
+ */
+
+using System;
+
+namespace SelectionSort
+{
+    class sSort
+    {
+        private int index = 0;
+        private int current_lowest = 0;
+        private int[] nlist = { 2, 5, 3, 1, 0, 4 };
+
+        public void Sort()
+        {
+            for(int n=0; n<nlist.Length; n++)
+            {                
+                for(int i=index; i<nlist.Length; i++)
+                {
+                    if (nlist[current_lowest] > nlist[i])
+                        current_lowest = i;
+                }
+
+                int a = nlist[index];
+                int b = nlist[current_lowest];
+
+                nlist[index] = b;
+                nlist[current_lowest] = a;
+
+                index += 1;
+                current_lowest = index;
+            }
+        }
+
+        public override string ToString()
+        {
+            string n = "{ ";
+
+            for(int i=0; i<nlist.Length; i++)
+            {
+                n += nlist[i] + ((i==nlist.Length-1) ? " }" : ", ");
+            }
+            return n;
+        }
+    }
+}
